66d18ccbdcd254e385cf1abd5647bf083860433a
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2025-03-30 01:37:24 +0530

    sorting algorithm updated

--- day6/activitySelection.c
+++ day6/activitySelection.c
@@ -1,45 +1,63 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// void sort(int **arr, int n)
+// {
+//     int i, j;
+//     int *temp;
+//     for (i = 0; i < n - 1; i++)
+//     {
+//         for (j = 0; j < n - i - 1; j++)
+//         {
+//             if ((arr[j][2] > arr[j + 1][2]) || (arr[j][2] == arr[j + 1][2] && arr[j][1] > arr[j + 1][1]))
+//             {
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
 
 void sort(int **arr, int n)
 {
-    int i, j;
-    int *temp;
-    for (i = 0; i < n - 1; i++)
+  int i, j;
+  int *temp;
+  for (i = 1; i < n; i++)
+  {
+    temp = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && (arr[j][2] > temp[2] || (arr[j][2] == temp[2] && arr[j][1] > temp[1])))
     {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if ((arr[j][2] > arr[j + 1][2]) || (arr[j][2] == arr[j + 1][2] && arr[j][1] > arr[j + 1][1]))
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
+    arr[j + 1] = temp;
+  }
 }
 
-int* avtselec(int** arr, int n)
+int *avtselec(int **arr, int n)
 {
-  sort(arr,n);
-  int i=0,m,k=1;
-  arr[0][3]=k;
+  sort(arr, n);
+  int i = 0, m, k = 1;
+  arr[0][3] = k;
   k++;
-  for(m=1;m<n;m++)
+  for (m = 1; m < n; m++)
   {
-    if(arr[i][2]<arr[m][1])
+    if (arr[i][2] < arr[m][1])
     {
-      i=m;
-      arr[i][3]=k;
+      i = m;
+      arr[i][3] = k;
       k++;
     }
   }
-  
-  int* ans = (int*)malloc(n*sizeof(int));
-  for(i=0;i<n;i++)
+
+  int *ans = (int *)malloc(n * sizeof(int));
+  for (i = 0; i < n; i++)
   {
-    ans[arr[i][0]]=arr[i][3];
+    ans[arr[i][0]] = arr[i][3];
   }
   return ans;
 }
@@ -47,11 +65,11 @@ int main()
 {
   int n;
   printf("Enter number of activity ");
-  scanf("%d",&n);
-  
-  int** avt = (int**)malloc(n*sizeof(int*));
-  
-  if(avt== NULL)
+  scanf("%d", &n);
+
+  int **avt = (int **)malloc(n * sizeof(int *));
+
+  if (avt == NULL)
   {
     printf("array creation failed \n ");
     return 0;
@@ -59,36 +77,34 @@ int main()
   else
   {
     int i;
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-      avt[i]=(int*)malloc(4*sizeof(int));
+      avt[i] = (int *)malloc(4 * sizeof(int));
     }
-    
-    for(i=0;i<n;i++)
+
+    for (i = 0; i < n; i++)
     {
-      avt[i][0]=i;
+      avt[i][0] = i;
       printf("Enter start time : ");
-      scanf("%d",&avt[i][1]);
-      
+      scanf("%d", &avt[i][1]);
+
       printf("Enter end time : ");
-      scanf("%d",&avt[i][2]);
-      
-      avt[i][3]=0;
+      scanf("%d", &avt[i][2]);
+
+      avt[i][3] = 0;
     }
-    
+
     clock_t start = clock();
-    int* ans = avtselec(avt,n);
+    int *ans = avtselec(avt, n);
     clock_t end = clock();
-    
+
     printf("Ans vector = ");
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-      printf("%d ",ans[i]);
+      printf("%d ", ans[i]);
     }
     printf("\n");
-    printf("Time taken = %lf second\n",((double)(end-start)/CLOCKS_PER_SEC));
+    printf("Time taken = %lf second\n", ((double)(end - start) / CLOCKS_PER_SEC));
   }
   return 0;
-  
 }
-    
--- day6/fractionalKnapsack.c
+++ day6/fractionalKnapsack.c
@@ -1,54 +1,72 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void sort(float** arr, int n)
+// void sort(float** arr, int n)
+// {
+//   int i,j;
+//   float* temp;
+//   for(i=0;i<n-1;i++)
+//   {
+//     for(j=0;j<n-1-i;j++)
+//     {
+//       if(arr[j][2]<arr[j+1][2])
+//       {
+//         temp=arr[j];
+//         arr[j]=arr[j+1];
+//         arr[j+1]=temp;
+//       }
+//     }
+//   }
+// }
+
+void sort(float **arr, int n)
 {
-  int i,j;
-  float* temp;
-  for(i=0;i<n-1;i++)
+  int i, j;
+  float *temp;
+  for (i = 1; i < n; i++)
   {
-    for(j=0;j<n-1-i;j++)
+    temp = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j][2] < temp[2])
     {
-      if(arr[j][2]<arr[j+1][2])
-      {
-        temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-      }
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
+    arr[j + 1] = temp;
   }
 }
 
-float* fknap(float** arr, int n, int maxw)
+float *fknap(float **arr, int n, int maxw)
 {
   int i;
-  for(i=0;i<n;i++)
+  for (i = 0; i < n; i++)
   {
-    arr[i][2]=arr[i][1]/arr[i][0];
+    arr[i][2] = arr[i][1] / arr[i][0];
   }
-  sort(arr,n);
-  
-  for(i=0;i<n;i++)
+  sort(arr, n);
+
+  for (i = 0; i < n; i++)
   {
-    if(arr[i][0]<=maxw)
+    if (arr[i][0] <= maxw)
     {
-      arr[i][3]=1;
-      maxw-=arr[i][0];
+      arr[i][3] = 1;
+      maxw -= arr[i][0];
     }
     else
     {
-      arr[i][3]=maxw/arr[i][0];
-      maxw=0;
+      arr[i][3] = maxw / arr[i][0];
+      maxw = 0;
     }
-    if(maxw==0)
-    break;
+    if (maxw == 0)
+      break;
   }
-  
-  float* ans = (float*)calloc(n,sizeof(float));
-  for(i=0;i<n;i++)
+
+  float *ans = (float *)calloc(n, sizeof(float));
+  for (i = 0; i < n; i++)
   {
-    ans[(int)arr[i][4]-1]=arr[i][3];
+    ans[(int)arr[i][4] - 1] = arr[i][3];
   }
   return ans;
 }
@@ -56,45 +74,45 @@ int main()
 {
   int n;
   printf("Enter no of weights : ");
-  scanf("%d",&n);
-  
-  float** w = (float**)malloc(n*sizeof(float*));
-  if(w== NULL)
+  scanf("%d", &n);
+
+  float **w = (float **)malloc(n * sizeof(float *));
+  if (w == NULL)
   {
     printf("Error creating array :");
     return 0;
   }
   else
   {
-    int i,maxwt;
-    for(i=0;i<n;i++)
+    int i, maxwt;
+    for (i = 0; i < n; i++)
     {
-      w[i]=(float*)calloc(5,sizeof(float));
+      w[i] = (float *)calloc(5, sizeof(float));
     }
-    
-    for(i=0;i<n;i++)
+
+    for (i = 0; i < n; i++)
     {
       printf("Enter Weight : ");
-      scanf("%f",&w[i][0]);
+      scanf("%f", &w[i][0]);
       printf("Enter Profit : ");
-      scanf("%f",&w[i][1]);
-      w[i][4]=i+1;
+      scanf("%f", &w[i][1]);
+      w[i][4] = i + 1;
     }
-    
+
     printf("Enter Max weight in knapsack : ");
-    scanf("%d",&maxwt);
-    
+    scanf("%d", &maxwt);
+
     clock_t start = clock();
-    float* ans = fknap(w,n,maxwt);
+    float *ans = fknap(w, n, maxwt);
     clock_t end = clock();
-    
+
     printf("Ans vector = ");
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-      printf("%f ",ans[i]);
+      printf("%f ", ans[i]);
     }
     printf("\n");
-    printf("Time taken = %lf second\n",((double)(end-start)/CLOCKS_PER_SEC));
+    printf("Time taken = %lf second\n", ((double)(end - start) / CLOCKS_PER_SEC));
   }
   return 0;
 }
